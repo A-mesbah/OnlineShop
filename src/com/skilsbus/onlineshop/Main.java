a95ac7a775a7be84c0a1c4a0f2a88701832d9016
Ahmed <mesbahes66@gmail.com>
2021-01-21 18:18:26 +0100

    hallo

--- src/com/skilsbus/onlineshop/Main.java
+++ src/com/skilsbus/onlineshop/Main.java
@@ -1,15 +1,7 @@
 package com.skilsbus.onlineshop;
 
-import product.Product;
-import product.SoftProduct;
-
 public class Main {
     public static void main(String[] args) {
-        Product product0 = new SoftProduct("windows", "software", 10, 2, 1, 15) {
-        };
-        Visitor visitor = new Visitor("ahmed ", "ahmed@123");
-        visitor.getShopingCart().addProduct(product0);
-        Website website = new Website();
-        website.addVisitor("Ahmed","ajhjb");
+
     }
 }
